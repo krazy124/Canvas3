7394dbf5f97a3abcbc98a2bd412c487e00940703
William Meredith <william.meredith4@gmail.com>
2023-10-24 13:23:04 -0500

    typo

--- input.js
+++ input.js
@@ -2,12 +2,12 @@ export class InputHandler {
   constructor() {
     this.keys = [];
     window.addEventListener("keydown", (e) => {
-      console.log(e.key, this.keys);
       if (e.key === "ArrowDown" && this.keys.indexOf(e.key) === -1) {
         this.keys.push(e.key);
       }
+      console.log(e.key, this.keys);
     });
-    windows.addEventListener("keyup", (e) => {
+    window.addEventListener("keyup", (e) => {
       if (e.key === "ArrowDown") {
         this.keys.splice(this.keys.indexOf(e.key), 1);
       }
