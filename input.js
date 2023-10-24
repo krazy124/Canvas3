8ddceb457c6d4085e1134a84353052a26488cc1d
William Meredith <william.meredith4@gmail.com>
2023-10-24 13:18:19 -0500

    added  2 event listeners. one to key track of when a key is pushed and one for whrn it is release. when it is pushed is is added to the array when it is relaesed it is removed from the array

--- input.js
+++ input.js
@@ -2,7 +2,16 @@ export class InputHandler {
   constructor() {
     this.keys = [];
     window.addEventListener("keydown", (e) => {
-      console.log(e.key);
+      console.log(e.key, this.keys);
+      if (e.key === "ArrowDown" && this.keys.indexOf(e.key) === -1) {
+        this.keys.push(e.key);
+      }
+    });
+    windows.addEventListener("keyup", (e) => {
+      if (e.key === "ArrowDown") {
+        this.keys.splice(this.keys.indexOf(e.key), 1);
+      }
+      console.log(e.key, this.keys);
     });
   }
 }
