88ceb5e91ca3fdbdb3e547f34961f6cab9443739
William Meredith <william.meredith4@gmail.com>
2023-10-23 22:56:01 -0500

    made a correctiont to img on index.html. Replaced red box with creepy dog.

--- player.js
+++ player.js
@@ -4,12 +4,21 @@ export class Player {
     this.width = 100;
     this.height = 91.3;
     this.x = 0;
-    this.y = 100;
     this.y = this.game.height - this.height;
+    this.image = document.getElementById("player");
   }
   update() {}
   draw(context) {
-    context.fillStyle = "red";
-    context.fillRect(this.x, this.y, this.width, this.height);
+    context.drawImage(
+      this.image,
+      0,
+      0,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
