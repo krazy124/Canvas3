b3ff37768528f5249d8ce124a685b4e6c73eef76
William Meredith <william.meredith4@gmail.com>
2023-10-24 16:05:47 -0500

    Added left Arrow and right arrow and enter to eventlisteners

--- input.js
+++ input.js
@@ -3,7 +3,11 @@ export class InputHandler {
     this.keys = [];
     window.addEventListener("keydown", (e) => {
       if (
-        (e.key === "ArrowDown" || e.key === "ArrowUp") &&
+        (e.key === "ArrowDown" ||
+          e.key === "ArrowUp" ||
+          e.key === "ArrowLeft" ||
+          e.key === "ArrowRight" ||
+          e.key === "Enter") &&
         this.keys.indexOf(e.key) === -1
       ) {
         this.keys.push(e.key);
@@ -11,7 +15,13 @@ export class InputHandler {
       console.log(e.key, this.keys);
     });
     window.addEventListener("keyup", (e) => {
-      if (e.key === "ArrowDown" || e.key === "ArrowUp") {
+      if (
+        e.key === "ArrowDown" ||
+        e.key === "ArrowUp" ||
+        e.key === "ArrowLeft" ||
+        e.key === "ArrowRight" ||
+        e.key === "Enter"
+      ) {
         this.keys.splice(this.keys.indexOf(e.key), 1);
       }
       console.log(e.key, this.keys);
