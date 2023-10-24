d8db7bf732422e897baf2d9a80124d248491daf3
William Meredith <william.meredith4@gmail.com>
2023-10-24 16:31:51 -0500

    wrote onground module which checks to see if the player is on the ground. This will be checked an things will be allowed if play is on the ground versus in the air.

--- player.js
+++ player.js
@@ -19,6 +19,8 @@ export class Player {
     if (this.x < 0) this.x = 0;
     if (this.x > this.game.width - this.width)
       this.x = this.game.width - this.width;
+    //vertical movement
+    this.y += this.vy;
   }
   draw(context) {
     context.drawImage(
@@ -32,5 +34,8 @@ export class Player {
       this.width,
       this.height
     );
+  }
+  onground() {
+    return this.y >= this.game.height - this.height;
   }
 }
