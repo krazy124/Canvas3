09a23811222b9996751774a839b5721839f29188
William Meredith <william.meredith4@gmail.com>
2023-10-24 16:24:05 -0500

    changed update so player cannot move off screen

--- player.js
+++ player.js
@@ -15,6 +15,9 @@ export class Player {
     if (input.includes("ArrowRight")) this.speed = this.maxSpeed;
     else if (input.includes("ArrowLeft")) this.speed = -this.maxSpeed;
     else this.speed = 0;
+    if (this.x < 0) this.x = 0;
+    if (this.x > this.game.width - this.width)
+      this.x = this.game.width - this.width;
   }
   draw(context) {
     context.drawImage(
