4a501f3f9797b21544ec346ed49552c497e1aa80
William Meredith <william.meredith4@gmail.com>
2023-10-24 15:52:01 -0500

    added ArrowUp to keydown listener and keyup listener

--- input.js
+++ input.js
@@ -2,13 +2,16 @@ export class InputHandler {
   constructor() {
     this.keys = [];
     window.addEventListener("keydown", (e) => {
-      if (e.key === "ArrowDown" && this.keys.indexOf(e.key) === -1) {
+      if (
+        (e.key === "ArrowDown" || e.key === "ArrowUp") &&
+        this.keys.indexOf(e.key) === -1
+      ) {
         this.keys.push(e.key);
       }
       console.log(e.key, this.keys);
     });
     window.addEventListener("keyup", (e) => {
-      if (e.key === "ArrowDown") {
+      if (e.key === "ArrowDown" || e.key === "ArrowUp") {
         this.keys.splice(this.keys.indexOf(e.key), 1);
       }
       console.log(e.key, this.keys);
