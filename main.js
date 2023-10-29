ed381b9e6cb7b1a35a75ebc69033d05fcd1abcd7
William Meredith <william.meredith4@gmail.com>
2023-10-28 22:33:05 -0500

    Fixed some bugs and added the 5 layers of the backgound

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { Player } from './player.js';
 import { InputHandler } from './input.js';
+import { Background } from './background.js';
 
 //load events makes sure all resources are loaded and avaiable before running any code
 window.addEventListener('load', function () {
@@ -15,17 +16,20 @@ window.addEventListener('load', function () {
     constructor(width, height) {
       this.width = width;
       this.height = height;
-      this.groundMargin = 50;
+      this.groundMargin = 81;
       this.speed = 3;
+      this.background = new Background(this);
       this.player = new Player(this); //Makes a new player class instance. So when a game is created a player is created also. Because this new player class instance is inside of game class we pass Player "this" which points to the game object it is inside of.Now we have access to all of Players methods and object
       this.input = new InputHandler(); //used from inputhandler in input.js
     }
     //Runs for every animation frame and triggers any calculations that needs to happen.
     update(deltaTime) {
+      this.background.update();
       this.player.update(this.input.keys, deltaTime); //The array from input.js that keep track of the current input
     }
     //Draws all our images and scores
     draw(context) {
+      this.background.draw(context);
       this.player.draw(context); //Calls Player classes draw method
     }
   }
